2bdb208a329936aa48777487f6a695397f04c2e8
tommy771004 <tommy771004@gmail.com>
2026-04-28 09:35:22 +0800

    Update ChartWidget.tsx

--- src/components/ChartWidget.tsx
+++ src/components/ChartWidget.tsx
@@ -358,7 +358,8 @@ tickMarkFormatter: (time: number) => {
           month: '2-digit',
           day: '2-digit',
           hour: '2-digit',
-          minute: '2-digit'
+          minute: '2-digit',
+          timeZone: 'Asia/Taipei',
         });
         
         setHoverData({
